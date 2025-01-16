e1a29f6c564c1d918283ea1942a3ff115208b9c9
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-01-16 21:47:31 +0530

    header fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,11 @@ import classNames from 'classnames';
 const Header = () => {
   const sticky = useStickyHeader(40);
   return (
+    // <div className={classNames(styles.header, { [styles.sticky]: sticky })}>
+    //   Header
+    // </div>
     <div className={classNames(styles.header, { [styles.sticky]: sticky })}>
-      Header
+      hesafe
     </div>
   );
 };
