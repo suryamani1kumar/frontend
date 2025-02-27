f22e34d708d2b5bbdb3a55a8d8a82422477fe1f9
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-02-27 07:42:41 +0530

    Add blog

--- src/components/common/AttraCard/index.js
+++ src/components/common/AttraCard/index.js
@@ -3,11 +3,89 @@ import Link from 'next/link';
 import React, { useRef, useState } from 'react';
 import { CiCalendar } from 'react-icons/ci';
 
+const Data = [
+  {
+    content:
+      'the towering Petronas Twin Towers to the historic temples and delicious street food ...',
+    metaTitle: '',
+    metaDescription: '',
+    metaKeyword: '',
+    pageUrl: '',
+    heading: 'Top Places to visit in India',
+    category: '',
+    status: '',
+  },
+  {
+    content:
+      'the towering Petronas Twin Towers to the historic temples and delicious street food ...',
+    metaTitle: '',
+    metaDescription: '',
+    metaKeyword: '',
+    pageUrl: '',
+    heading: 'Top Places to visit in India',
+    category: '',
+    status: '',
+  },
+  {
+    content:
+      'the towering Petronas Twin Towers to the historic temples and delicious street food ...',
+    metaTitle: '',
+    metaDescription: '',
+    metaKeyword: '',
+    pageUrl: '',
+    heading: 'Top Places to visit in India',
+    category: '',
+    status: '',
+  },
+  {
+    content:
+      'the towering Petronas Twin Towers to the historic temples and delicious street food ...',
+    metaTitle: '',
+    metaDescription: '',
+    metaKeyword: '',
+    pageUrl: '',
+    heading: 'Top Places to visit in India',
+    category: '',
+    status: '',
+  },
+  {
+    content:
+      'the towering Petronas Twin Towers to the historic temples and delicious street food ...',
+    metaTitle: '',
+    metaDescription: '',
+    metaKeyword: '',
+    pageUrl: '',
+    heading: 'Top Places to visit in India',
+    category: '',
+    status: '',
+  },
+  {
+    content:
+      'the towering Petronas Twin Towers to the historic temples and delicious street food ...',
+    metaTitle: '',
+    metaDescription: '',
+    metaKeyword: '',
+    pageUrl: '',
+    heading: 'Top Places to visit in India',
+    category: '',
+    status: '',
+  },
+  {
+    content:
+      'the towering Petronas Twin Towers to the historic temples and delicious street food ...',
+    metaTitle: '',
+    metaDescription: '',
+    metaKeyword: '',
+    pageUrl: '',
+    heading: 'Top Places to visit in India',
+    category: '',
+    status: '',
+  },
+];
 const AttraCard = () => {
   const swiperRef = useRef(null);
   const handleprevButton = () => {
     const card = swiperRef.current;
-    console.log('card', card);
     if (card) {
       const width = card.clientWidth;
       card.scrollLeft -= width;
@@ -15,7 +93,6 @@ const AttraCard = () => {
   };
   const handlenextButton = () => {
     const card = swiperRef.current;
-    console.log('card', card);
     if (card) {
       const width = card.clientWidth;
       card.scrollLeft += width;
@@ -25,35 +102,30 @@ const AttraCard = () => {
     <div className="relative">
       <div className="swiperRootContainer" ref={swiperRef}>
         <div className="wrapper">
-          {/* https://github.com/virajj014/productslider-react */}
-          {Array(9)
-            .fill(0)
-            .map((item, i) => (
-              <div className="card" key={i}>
-                <Image
-                  src="/destination-img/kuala-lumpur.webp"
-                  className="card__img"
-                  alt="kuala-lumpur"
-                  width={0}
-                  height={0}
-                />
-                <div className="card__body">
-                  <h2 className="card__title">Kuala Lumpur {i}</h2>
-                  <p className="card__description">
-                    the towering Petronas Twin Towers to the historic temples
-                    and delicious street food ...
+          {/* https://www.travelogyindia.com/ */}
+          {Data.map((item, i) => (
+            <div className="card" key={i}>
+              <Image
+                src="/destination-img/kuala-lumpur.webp"
+                className="card__img"
+                alt="kuala-lumpur"
+                width={0}
+                height={0}
+              />
+              <div className="card__body">
+                <h2 className="card__title">{item.heading}</h2>
+                <p className="card__description">{item.content}</p>
+                <div className="card_read">
+                  <Link href="attractions/kuala-lumpur" className="readMore">
+                    Read Article
+                  </Link>
+                  <p className="card_releaseDate">
+                    <CiCalendar /> Aug 21
                   </p>
-                  <div className="card_read">
-                    <Link href="attractions/kuala-lumpur" className="readMore">
-                      Read Article
-                    </Link>
-                    <p className="card_releaseDate">
-                      <CiCalendar /> Aug 21
-                    </p>
-                  </div>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       </div>
       <button
