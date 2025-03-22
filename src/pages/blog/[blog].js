fa666e08538368c6a1b6937c73d04062eb9fab77
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-03-22 11:09:53 +0530

    add blogDetails  api

--- src/pages/blog/[blog].js
+++ src/pages/blog/[blog].js
@@ -1,7 +1,48 @@
-import React from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 
 const BlogDetails = () => {
-  return <div>BlogDetails</div>;
+  const router = useRouter();
+  const { query, isReady } = router; // Ensure the query is ready before using it
+  const [blogData, setBlogData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `http://localhost:5000/api/blog?pageurl=${query.blog}`,
+        {
+          headers: {
+            'api-key': 'your-valid-api-key',
+          },
+        }
+      );
+      setBlogData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setError('Failed to load blog details');
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (!isReady || !query.blog) return; // Ensure query param exists and router is ready
+
+    fetchData();
+  }, [isReady, query.blog]); // Only fetch when router is ready and `pageurl` exists
+  console.log('blogData', blogData);
+  return (
+    <div className="container">
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {blogData && (
+        <div dangerouslySetInnerHTML={{ __html: blogData.blog.content }} />
+      )}
+    </div>
+  );
 };
 
 export default BlogDetails;
