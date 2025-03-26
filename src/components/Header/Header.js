892ce8a682db027c91e0c003b46e8efff1a2596b
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-26 17:00:06 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import styles from './header.module.scss';
-import { useStickyHeader } from '@/hooks/useStickyHeader';
-import classNames from 'classnames';
+import React, { useState } from "react";
+import styles from "./header.module.scss";
+import { useStickyHeader } from "@/hooks/useStickyHeader";
+import classNames from "classnames";
+import Image from "next/image";
+import Link from "next/link";
+import { IoSearchSharp } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
+import { useDeviceType } from "@/hooks/useDevicetype";
+import Drawer from "@mui/material/Drawer";
+
 const Header = () => {
   const sticky = useStickyHeader(40);
+  const device = useDeviceType();
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const toggleDrawer = () => {
+    setOpenDrawer(!openDrawer);
+  };
+
   return (
-    <div className={'container'}>
-      <div className={classNames(styles.header, { [styles.sticky]: sticky })}>
-        Triploom
+    <header
+      className={classNames(styles.headerContainer, {
+        [styles.sticky]: sticky,
+      })}
+    >
+      <div className="container flexcontainer">
+        <div>
+          <Link href={"/"}>
+            <Image
+              src="https://dummyimage.com/130x40/878487/fff"
+              height={40}
+              width={130}
+              alt="logo"
+            />
+          </Link>
+        </div>
+        {device !== "Desktop" && (
+          <div className="flexcontainer">
+            <div className={styles.Headsearch}>
+              <IoSearchSharp />
+            </div>
+            <div className={styles.sideMenu} onClick={toggleDrawer}>
+              <IoMenu />
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+      {openDrawer && (
+        <Drawer open={openDrawer} onClose={toggleDrawer}>
+          <ul>
+            <li>Hello world</li>
+          </ul>
+        </Drawer>
+      )}
+    </header>
   );
 };
 
