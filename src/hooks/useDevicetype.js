d56e6d3433eaddc52390ecad446a3a61664a2c3f
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-26 16:46:47 +0530

    Update useDevicetype.js

--- src/hooks/useDevicetype.js
+++ src/hooks/useDevicetype.js
@@ -1,20 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export const useDeviceType = () => {
-  const [deviceType, setDeviceType] = useState('Desktop');
+  const [deviceType, setDeviceType] = useState("Desktop");
 
   useEffect(() => {
     const checkDevice = () => {
       if (window.innerWidth < 768) {
-        setDeviceType('Mobile');
+        setDeviceType("Mobile");
+      } else if (window.innerWidth > 768 && window.innerWidth < 1024) {
+        setDeviceType("Tablet");
       } else {
-        setDeviceType('Desktop');
+        setDeviceType("Desktop");
       }
     };
 
     checkDevice();
-    window.addEventListener('resize', checkDevice);
-    return () => window.removeEventListener('resize', checkDevice);
+    window.addEventListener("resize", checkDevice);
+    return () => window.removeEventListener("resize", checkDevice);
   }, []);
 
   return deviceType;
