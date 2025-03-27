df2200d3883c77c04ea0707465a7454a991e6543
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-27 18:24:37 +0530

    Update SearchBar.js

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -5,6 +5,7 @@ import styles from './search.module.scss';
 const SearchBar = () => {
   return (
     <div className={styles.searchContainer}>
+  <h1>"Adventure, excitement, and unforgettable experiences—all in one place!"</h1>
       <div className={styles.searchInput}>
         <IoSearch />
         <input placeholder="Places to go, things to do ..." />
