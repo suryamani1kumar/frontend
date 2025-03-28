2420325dd40af6b2bd1b8ecd7da361e13d056727
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-28 14:04:44 +0530

    Update profileSlice.js

--- src/redux/reducers/profileSlice.js
+++ src/redux/reducers/profileSlice.js
@@ -1,1 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+const profileSlice = createSlice({
+  name: "profile",
+  initialState: {
+    name: null,
+  },
+  reducers: {
+    SET_NAME: (state, action) => {
+      state.name = action.payload;
+    },
+  },
+});
+export const { SET_NAME } = profileSlice.actions;
+export default profileSlice.reducer;
 
