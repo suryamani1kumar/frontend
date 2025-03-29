656fae04443237c454c6e51c8c08146cea9c2d41
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-03-29 07:29:04 +0530

    header toggle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,34 @@
-import React, { useState } from "react";
-import styles from "./header.module.scss";
-import { useStickyHeader } from "@/hooks/useStickyHeader";
-import classNames from "classnames";
-import Image from "next/image";
-import Link from "next/link";
-import { IoSearchSharp } from "react-icons/io5";
-import { IoMenu } from "react-icons/io5";
-import { useDeviceType } from "@/hooks/useDevicetype";
-import Drawer from "@mui/material/Drawer";
+import React, { useEffect, useState } from 'react';
+import styles from './header.module.scss';
+import { useStickyHeader } from '@/hooks/useStickyHeader';
+import classNames from 'classnames';
+import Image from 'next/image';
+import Link from 'next/link';
+import { IoSearchSharp } from 'react-icons/io5';
+import { IoMenu } from 'react-icons/io5';
+import { useDeviceType } from '@/hooks/useDevicetype';
+import Drawer from '@mui/material/Drawer';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
   const sticky = useStickyHeader(40);
   const device = useDeviceType();
   const [openDrawer, setOpenDrawer] = useState(false);
 
-  const toggleDrawer = () => {
-    setOpenDrawer(!openDrawer);
+  const toggleDrawerOn = () => {
+    setOpenDrawer(true);
   };
+  const toggleDrawerOff = () => {
+    setOpenDrawer(false);
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', toggleDrawerOff);
+    return () => {
+      router.events.off('routeChangeComplete', toggleDrawerOff);
+    };
+  }, [router]);
 
   return (
     <header
@@ -26,7 +38,7 @@ const Header = () => {
     >
       <div className="container flexcontainer">
         <div>
-          <Link href={"/"}>
+          <Link href={'/'}>
             <Image
               src="https://dummyimage.com/130x40/878487/fff"
               height={40}
@@ -35,46 +47,36 @@ const Header = () => {
             />
           </Link>
         </div>
-        {device !== "Desktop" && (
+        {device !== 'Desktop' && (
           <div className="flexcontainer">
             <div className={styles.Headsearch}>
               <IoSearchSharp />
             </div>
-            <div className={styles.sideMenu} onClick={toggleDrawer}>
+            <div className={styles.sideMenu} onClick={toggleDrawerOn}>
               <IoMenu />
             </div>
           </div>
         )}
       </div>
       {openDrawer && (
-        <Drawer open={openDrawer} onClose={toggleDrawer}>
+        <Drawer open={openDrawer} onClose={toggleDrawerOff}>
           <div className={styles.menu_container}>
             <div className={styles.menu_header}>
-              <Link href={"/"}>
+              <Link href={'/'}>
                 <Image
-                  src="https://dummyimage.com/130x40/878487/fff"
-                  height={40}
-                  width={130}
+                  src="https://dummyimage.com/180x50/878487/fff"
+                  height={0}
+                  width={0}
                   alt="logo"
                 />
               </Link>
-              <span className={styles.close_btn}>&times;</span>
+              <span className={styles.close_btn} onClick={toggleDrawerOff}>
+                &times;
+              </span>
             </div>
             <ul className={styles.menu_list}>
               <li className={styles.menu_item}>
-                <i>✈️</i> Flights
-              </li>
-              <li className={styles.menu_item}>
-                <i>🏨</i> Hotels
-              </li>
-              <li className={styles.menu_item}>
-                <i>🏖️</i> Vacation
-              </li>
-              <li className={styles.menu_item}>
-                <i>🚗</i> Car Rental
-              </li>
-              <li className={styles.menu_item}>
-                <i>🔗</i> Transfers
+                <Link href={'/blog'}>Blog</Link>
               </li>
             </ul>
           </div>
