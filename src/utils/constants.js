47dd3c1e483c8131fb39e7dde517447f2ee022da
Harsh <harshjoshi2k@gmail.com>
2025-04-06 21:58:03 +0530

    navbar desktop view complete without functionality

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const NavBarItems = [
+  {
+    id: 1,
+    name: "Discover",
+    link: "/discover",
+  },
+  {
+    id: 2,
+    name: "Book a guide",
+    link: "/book-a-guide",
+  },
+  {
+    id: 3,
+    name: "Blog",
+    link: "/blog",
+  },
+];
