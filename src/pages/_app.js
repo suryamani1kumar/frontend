22061f6b8fcf8f396867822b31d0ff505e7bad3b
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-05-16 18:10:29 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
