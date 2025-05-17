f6902c67f7b5b015aed8a6d36524aa5d7cd61fe6
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-05-17 12:07:32 +0530

    change in contact section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,47 +3,44 @@ import { FaTelegramPlane } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaPhone } from "react-icons/fa6";
 import { FaEnvelopeOpen } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
+import styles from "./footer.module.scss"
 import Link from "next/link";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="footer-section">
+    <footer className={styles.footer_section}>
       <div className="container">
-        <div className="footer-cta ">
-          <div className="row">
-            <div className="col-xl-4 col-md-4 mb-30">
-              <div className="single-cta">
-                <FaLocationDot />
-                <div className="cta-text">
-                  <h4>Find us</h4>
-                  <span>1010 Avenue, sw 54321, chandigarh</span>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-4 mb-30">
-              <div className="single-cta">
-                <FaPhone />
-                <div className="cta-text">
-                  <h4>Call us</h4>
-                  <Link href="tel:+XXXX XXXX X">XXXX XXXX X</Link>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-4 mb-30">
-              <div className="single-cta">
-                <FaEnvelopeOpen />
-                <div className="cta-text">
-                  <h4>Mail us</h4>
-                  <Link href="mailto:+XXXX XXXX X">triploom@info.com</Link>
-                </div>
-              </div>
+        <div className={styles.footer_cta}>
+
+          <div className={styles.single_cta}>
+            <FaLocationDot />
+            <div className={styles.cta_text}>
+              <h4>Find us</h4>
+              <span>1010 Avenue, sw 54321, chandigarh</span>
             </div>
           </div>
+
+          <div className={styles.single_cta}>
+            <FaPhone />
+            <div className={styles.cta_text}>
+              <h4>Call us</h4>
+              <Link href="tel:+XXXX XXXX X">XXXX XXXX X</Link>
+            </div>
+          </div>
+
+          <div className={styles.single_cta}>
+            <FaEnvelopeOpen />
+            <div className={styles.cta_text}>
+              <h4>Mail us</h4>
+              <Link href="mailto:+XXXX XXXX X">triploom@info.com</Link>
+            </div>
+          </div>
+
+
         </div>
-        <div className="footer-content">
+        <div className={styles.footer_content}>
           <div className="row">
-            
+
             <div className="col-xl-8 col-lg-8 col-md-6 mb-30">
               <div className="footer-widget">
                 <div className="footer-widget-heading">
@@ -77,7 +74,7 @@ const Footer = () => {
                   <form action="#">
                     <input type="text" placeholder="Email Address" />
                     <button>
-                         <FaTelegramPlane />
+                      <FaTelegramPlane />
                     </button>
                   </form>
                 </div>
@@ -86,7 +83,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="copyright-area">
+      <div className={styles.copyright_area}>
         <div className="container">
           <div className="row">
             <div className="col-xl-6 col-lg-6 text-center text-lg-left">
