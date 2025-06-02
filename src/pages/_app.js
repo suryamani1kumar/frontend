3589692448fda638bec4a831d9bd80b5a09c9470
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-06-02 17:52:13 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,13 @@ import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
-import TrackingPixel from "@/component/trackingpixel/TrackingPixel";
+import TrackingPixel from "@/components/trackingpixel/TrackingPixel";
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Layout>
-     <TrackingPixel />
+        <TrackingPixel />
         <Component {...pageProps} />
       </Layout>
     </Provider>
