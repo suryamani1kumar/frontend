d71bb6fa18a764e27ed7547ce654ee0f78b18fda
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-06-14 16:09:31 +0530

    Add blog page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,8 @@
-import { getData } from '@/service/fetchData';
-import Link from 'next/link';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { getData } from "@/service/fetchData";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Blog = () => {
   const dispatch = useDispatch();
@@ -9,9 +10,20 @@ const Blog = () => {
   useEffect(() => {
     dispatch(getData(`allBlog?page=1&limit=4`));
   }, []);
-  console.log("data", data.
-    blog)
-  return <div className="container">{data?.blog?.map((item,i) => <Link href={`/blog/${item.pageUrl}`} key={i}>{item.pageUrl}</Link>)}</div>;
+  console.log("data", data.blog);
+  return (
+    <div className="container">
+      {data?.blog?.map((item, i) => (
+        <div key={i}>
+          {console.log("item", item)}
+          <Link href={`/blog/${item.pageUrl}`} key={i}>
+            <Image src={item.images[0]} height={400} width={400} />
+            {item.heading}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Blog;
