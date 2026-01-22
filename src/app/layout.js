1f79455a3bef1a6aa3b083176529d73553d72714
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-22 19:32:40 +0530

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,25 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import Providers from "@/components/Provider/Providers";
+import TrackingPixel from "@/components/trackingpixel/TrackingPixel";
+import "@/styles/globals.css";
+
+export const metadata = {
+  title: "My App",
+  description: "Frontend App Router",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          <Header />
+          <TrackingPixel />
+          <div className="overallMar">{children}</div>
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
+}
