6825de600ec68ea3b2e5ee258dba1ddd537337aa
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-22 19:30:54 +0530

    Delete src/pages directory

--- src/pages/attractions/[attractionPlace].js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const attractionPlace = () => {
-  return <div>attractionPlace</div>;
-};
-
-export default attractionPlace;
--- src/pages/terms-condition.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const TermsCondition = () => {
-
-  return <div>{process.env.NEXT_PUBLIC_APP_NAME}</div>;
-};
-
-export default TermsCondition;
