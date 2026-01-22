3fd78f9fbf6c60d34ee0a9a54890ad75dfa6d6a6
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-22 19:34:08 +0530

    Create Providers.js

---
+++ src/components/Provider/Providers.js
@@ -0,0 +1,9 @@
+// src/app/providers.js
+"use client";
+
+import { Provider } from "react-redux";
+import store from "@/redux/store";
+
+export default function Providers({ children }) {
+  return <Provider store={store}>{children}</Provider>;
+}
