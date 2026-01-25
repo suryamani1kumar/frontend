f59d914eb056f172e54336ec57f3ef92f26f0801
Harsh <harshjoshi2k@gmail.com>
2026-01-25 15:31:25 +0530

    add meta tag in about us section

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,5 +1,12 @@
 import React from "react";
 import AboutUsSection from "@/components/About/AboutUsSection";
+import { siteName } from "@/utils/constants";
+export const metadata = {
+  title: `About Us | ${siteName}  - Flights, Hotels & Travel Guides`,
+  description: `Discover ${siteName}, your ultimate travel companion. Book flights, hotels, explore attractions, and get expert travel guides for unforgettable trips worldwide.`,
+  keywords:
+    "Travel, Flight Booking, Hotel Booking, Tourist Attractions, Travel Guides, Vacation, Trips",
+};
 const AboutUs = () => {
   return <AboutUsSection />;
 };
