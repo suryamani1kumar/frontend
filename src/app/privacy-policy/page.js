a0c023f8de9f9f2fb3ddbf274fab3bf8ae5eec97
Harsh <harshjoshi2k@gmail.com>
2026-01-31 14:36:27 +0530

    working on privacy policy

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,8 @@
+import PrivacyPolicySection from "@/components/Privacypolicy/PrivacyPolicySection";
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return <PrivacyPolicySection />;
+};
+
+export default PrivacyPolicy;
---
+++ src/components/Privacypolicy/PrivacyPolicySection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PrivacyPolicySection = () => {
+  return <div>PrivacyPolicySection</div>;
+};
+
+export default PrivacyPolicySection;
