178e70d0b945c9548a59fdf5a183fb33107d9323
Harsh <harshjoshi2k@gmail.com>
2026-02-12 06:39:45 +0530

    working on blog oage

--- src/components/Blog/BlogDetailPageSection.js
+++ src/components/Blog/BlogDetailPageSection.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import dayjs from "dayjs";
 import { truncateText } from "@/utils/constants";
 const BlogDetailPageSection = ({ blog, bloglist }) => {
+  console.log("blog", blog);
   return (
     <>
       <div className="container">
@@ -14,10 +15,38 @@ const BlogDetailPageSection = ({ blog, bloglist }) => {
             src={blog.blog?.images?.url || "/destination-img/kuala-lumpur.webp"}
             className={styles.BlogDetails_Image}
             alt="kuala-lumpur"
-            width={900}
+            width={800}
             height={400}
             priority
           />
+        </div>
+        <div className={styles.BlogDetails_recentblog}>
+          <h5>Recent Blogs</h5>
+          {bloglist.blog.map((blg) => (
+            <div key={blg._id} className={styles.recent_blog_list}>
+              <Link href={`/blog/${blg.pageUrl}`}>
+                <div style={{ display: "flex" }}>
+                  <Image
+                    src={
+                      blg?.images?.url || "/destination-img/kuala-lumpur.webp"
+                    }
+                    width={85}
+                    height={70}
+                    alt={"Image"}
+                    style={{
+                      borderTopLeftRadius: "10px",
+                      borderBottomLeftRadius: "10px",
+                    }}
+                    priority
+                  />
+                  <div style={{ marginLeft: "10px" }}>
+                    <p>{dayjs(blg.createdAt).format("MMM D, YYYY")}</p>
+                    <p>{truncateText(blg.heading, 65)}</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
         <div style={{ display: "flex", gap: "20px" }}>
           <div className={styles.BlogDetails}>
@@ -28,34 +57,6 @@ const BlogDetailPageSection = ({ blog, bloglist }) => {
               dangerouslySetInnerHTML={{ __html: blog.blog.content }}
               className={styles.BlogDetailsDescr}
             />
-          </div>
-          <div className={styles.BlogDetails_recentblog}>
-            <h5>Recent Blogs</h5>
-            {bloglist.blog.map((blg) => (
-              <div key={blg._id} className={styles.recent_blog_list}>
-                <Link href={`/blog/${blg.pageUrl}`}>
-                  <div style={{ display: "flex" }}>
-                    <Image
-                      src={
-                        blg?.images?.url || "/destination-img/kuala-lumpur.webp"
-                      }
-                      width={85}
-                      height={70}
-                      alt={"Image"}
-                      style={{
-                        borderTopLeftRadius: "10px",
-                        borderBottomLeftRadius: "10px",
-                      }}
-                      priority
-                    />
-                    <div style={{ marginLeft: "10px" }}>
-                      <p>{dayjs(blg.createdAt).format("MMM D, YYYY")}</p>
-                      <p>{truncateText(blg.heading, 65)}</p>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            ))}
           </div>
         </div>
       </div>
