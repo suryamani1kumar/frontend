58bc8278137f97fda8395010e8e2903c889b7ed4
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-02-16 11:32:10 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,9 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Providers from "@/components/Provider/Providers";
-import TrackingPixel from "@/components/trackingpixel/TrackingPixel";
 import "@/styles/globals.css";
 
 export const dynamic = "force-dynamic";
-
-export const metadata = {
-  title: "My App",
-  description: "Frontend App Router",
-};
 
 export default function RootLayout({ children }) {
   return (
@@ -17,7 +11,6 @@ export default function RootLayout({ children }) {
       <body>
         <Providers>
           <Header />
-          <TrackingPixel />
           <div className="overallMar">{children}</div>
           <Footer />
         </Providers>
