7ca2f84fcd0cd609906b2aad4ecb4b796f377f09
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-03-02 17:52:06 +0530

    Update BlogList.js

--- src/components/Blog/BlogList.js
+++ src/components/Blog/BlogList.js
@@ -11,7 +11,7 @@ const BlogList = ({ blog }) => {
         <Card
           key={i}
           url={`/blog/${item.pageUrl}`}
-          img={item.images?.url || "/destination-img/kuala-lumpur.webp"}
+          img={item.images?.url || "/blog-generic.webp"}
           heading={truncateText(item.heading, 60)}
           content={truncateText(item.smallDescription, 157)}
           createDate={dayjs(item.createdAt).format("MMM D, YYYY")}
